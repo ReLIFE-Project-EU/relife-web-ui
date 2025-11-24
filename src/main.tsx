6f6b05baac699dc122c0263d888711bf71b3f2b9
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2025-11-24 11:57:00 +0100

    💄 Configure Mantine theme and use primary color variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import App from "./App.tsx";
+import { theme } from "./theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <App />
     </MantineProvider>
   </StrictMode>,
